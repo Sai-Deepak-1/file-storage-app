e7465eba3ec496392b68427f70001436f0d94e0e
sangeetha875 <sangeethatirunagari875@gmail.com>
2024-04-23 14:14:12 +0530

    Update files.ts

--- convex/files.ts
+++ convex/files.ts
@@ -1,5 +1,7 @@
 import { ConvexError, v } from "convex/values";
 import { mutation, query } from "./_generated/server";
+import { getUser } from "./users";
+import { error } from "console";
 
 export const createFile = mutation({
   args: {
@@ -12,6 +14,13 @@ export const createFile = mutation({
     if (!identity) {
       throw new ConvexError(" You Must be Logged In to Upload a File! ");
     }
+
+    const user = await getUser(ctx, identity.tokenIdentifier);
+
+    if (!user.orgIds.includes(args.orgId) && user.tokenIdentifier!=identity.tokenIdentifier) {
+      throw new ConvexError("You do not have access  to this Organization");
+    }
+
     await ctx.db.insert("files", {
       name: args.name,
       orgId: args.orgId,
